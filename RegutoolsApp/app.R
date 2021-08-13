e140b9084250cd039fcd09168f708d6cf91f6315
mruizros <mruizros@gmail.com>
2021-08-13 13:09:35 -0500

    Detalles

--- RegutoolsApp/app.R
+++ RegutoolsApp/app.R
@@ -13,7 +13,13 @@ e_coli_regulondb <-
     database_version = "1",
     genome_version = "1"
   )
-##################
+####### Datos iniciales Pepe
+e_coli_regulondb_Pchr <- regulondb(
+  database_conn = regulondb_conn,
+  organism = "chr",
+  database_version = "1",
+  genome_version = "1")
+
 
 ### Datos iniciales Monica
 names <- get_dataset(
@@ -35,23 +41,41 @@ ui <- fluidPage(
   titlePanel("Regutools"),
   
   sidebarLayout(
-    sidebarPanel(),
+    sidebarPanel(
+      sliderInput("genepos", label = "Genomics range:", min = 0,
+                  max = 4000000, value = c(5000, 10000))
+    ),
     
     mainPanel(
       tabsetPanel(
         
-        tabPanel("Genes dentro de intervalo",
-                 sliderInput("genepos", label = h3("Intervalo"), min = 2000, max = 4000000, 
-                             value = c(2000, 400000)),
-                 tableOutput("gene")
+        tabPanel("Genes within genomic ranges",
+                 fluidRow(column(12,dataTableOutput('gene')))
+                 
           
         ),
         
-        tabPanel("Genes regulados por transcritos",
+        tabPanel("Genomics elements",
+                 selectInput("variable", "Genomics element:",
+                             c("gene" = "gene",
+                               "promoter" = "promoter",
+                               "-10 promoter box" = "-10 promoter box",
+                               "-35 promoter box" = "-35 promoter box",
+                               "Regulatory Interaction" = "Regulatory Interaction",
+                               "sRNA interaction" = "sRNA interaction",
+                               "terminator" = "terminator"),
+                             multiple = T),
+                 plotOutput("Grafico"),
+                 tableOutput("data")
+          
+        ),
+        
+        
+        tabPanel("Genes regulated by transcripts",
                  selectizeInput('genes', label= "Seleccionar genes regulados", choices= genes, multiple= TRUE),
-                 radioButtons( 'ByTrans_Gene', label=h3("Ver por transcrito o por genes regulados"), 
-                               choices = c(Transcrito="Factor_transcripcion", Genes_regulados="Genes_regulados"), 
-                               selected = "Factor_transcripcion" ), 
+                 radioButtons( 'ByTrans_Gene', label=h3("See by:"), 
+                               choices = c(Transcription_factor="Transcription_factor", Genes_regulated="Genes_regulated"), 
+                               selected = "Transcription_factor" ), 
                  fluidRow(column(12,dataTableOutput('Regulatory'))))
         
         
@@ -79,10 +103,41 @@ server <- function(input, output) {
     interval = "posright")
   })
 
-  output$gene <- renderTable({
+  
+  ###### Pestaña 1
+  output$gene <- renderDataTable({
     a()
   })
   
+  ####### Pestaña 2
+  output$data <- renderTable({
+    
+    req(length(input$variable) > 0)
+    
+    start <- as.integer(input$genepos[1])
+    end <- as.integer(input$genepos[2])
+    grange <- GenomicRanges::GRanges("chr", IRanges::IRanges(start,end))
+    
+    
+    Result_range <- get_dna_objects(e_coli_regulondb, grange=grange, elements = c(input$variable))
+    
+    Result_range
+    
+  }, rownames = TRUE)
+  
+  
+  output$Grafico <- renderPlot({
+    
+    req(length(input$variable) > 0)
+    
+    start <- as.integer(input$genepos[1])
+    end <- as.integer(input$genepos[2])
+    grange <- GenomicRanges::GRanges("chr", IRanges::IRanges(start,end))
+    
+    plot_dna_objects(e_coli_regulondb_Pchr, grange=grange, elements = c(input$variable))
+  })
+  
+  ####### Pestaña 3
   output$Regulatory <- renderDataTable({
     
     
@@ -100,7 +155,7 @@ server <- function(input, output) {
     
     
     
-    if (input$ByTrans_Gene == "Factor_transcripcion") {
+    if (input$ByTrans_Gene == "Transcription_factor") {
       RegulatorsTrans
     } else {
       RegulatorsGene
